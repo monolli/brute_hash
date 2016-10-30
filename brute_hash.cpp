117a1b4cc0c5f28719bd6b0db6788c3002389298
Lucas Monteiro de Oliveira <lumonoli@hotmail.com>
2016-10-30 02:39:14 +0200

    loading the hash list

--- brute_hash.cpp
+++ brute_hash.cpp
@@ -6,3 +6,36 @@ E-mail: felipe.regis@outlook.com
 Outubro 2016
 AED II - Bechmark of hashing algorithms
 */
+
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define start = 32; //32 == space ASCII
+#define end = 126; //126 == ~ ASCII
+
+using namespace std;
+
+void load_hash(string name, vector<string>& l_hash){
+  cout<< "Name of the file containing the hash: " <<endl;
+  cin>>name;
+
+  ifstream input(name.c_str());   //create the file stream
+  string line;                    //temp line
+
+  while(getline(input,line)){     //recieve the line
+    l_hash.push_back(line);       //store the hash
+  }
+}
+
+int main(){
+  string name;
+  vector<string> hash_list;
+
+  load_hash(name,hash_list);
+
+
+  return 0;
+}
